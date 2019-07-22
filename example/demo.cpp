2cc9fe54b9a1751500ee0d0876ea751e2e38a169
Lornatang <shiyipaisizuo@163.com>
2019-07-22 13:53:09 +0800

    fix error

--- example/demo.cpp
+++ example/demo.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include "opencv4/opencv2/core.hpp"
 #include "opencv4/opencv2/highgui.hpp"
 #include "opencv4/opencv2/imgproc.hpp"
 
--- example/demo.py
+++ example/demo.py
@@ -1,15 +1,9 @@
+import pytesseract
 import cv2
-
-img = cv2.imread('./images/demo1.png')
-GrayImage = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
-CannyImage = cv2.Canny(GrayImage, 50, 150, apertureSize=3)
-
-ret, BinImage = cv2.threshold(CannyImage, 127, 255, cv2.THRESH_BINARY)
-contours, _ = cv2.findContours(
-    BinImage, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-
-cv2.imshow('bin', BinImage)
-cv2.drawContours(img, contours, -1, (0, 255, 0), 1)
-cv2.imshow('edge', img)
-cv2.waitKey(0)
+import matplotlib.pyplot as plt
+import dlib
+import matplotlib.patches as mpatches
+from skimage import io, draw, transform, color
+import numpy as np
+import pandas as pd
+import re
